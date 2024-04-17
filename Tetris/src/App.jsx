6694789c68ba110a55cd121a68ca070cad4416a1
Mateusz <u70_matdel_waw@technischools.com>
2024-04-17 08:44:38 +0200

    PierwszyCommit

---
+++ Tetris/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+function App() {
+  return (
+    <>
+      <div className="App">
+        <h1>Tetris</h1>
+        <h2>By Matteo</h2>
+      </div>
+    </>
+  );
+}
+
+export default App;
